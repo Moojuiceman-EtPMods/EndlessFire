2ae83db191a0064b4373f493ee9c7110a00bfef3
Alex Barrett <moojuiceman@hotmail.co.uk>
2023-01-06 08:50:50 +0000

    Initial port/fixes

---
+++ Plugin.cs
@@ -0,0 +1,29 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace EndlessFire
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        static ManualLogSource logger;
+
+        private void Awake()
+        {
+            // Plugin startup logic
+            logger = Logger;
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded");
+            Logger.LogInfo($"Patching...");
+            Harmony.CreateAndPatchAll(typeof(Plugin));
+            Logger.LogInfo($"Patched");
+        }
+
+        [HarmonyPatch(typeof(CampFire), "StopFire")]
+        [HarmonyPrefix]
+        static bool StopFire_Prefix()
+        {
+            return false;
+        }
+    }
+}
